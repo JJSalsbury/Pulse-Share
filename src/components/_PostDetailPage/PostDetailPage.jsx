f18907c5b3fc1c63cbe56809a739c044ee36685e
Brandon McCarty <brandon.m.mccarty12@gmail.com>
2022-05-17 10:58:19 -0500

    setup detsil router

--- src/components/_PostDetailPage/PostDetailPage.jsx
+++ src/components/_PostDetailPage/PostDetailPage.jsx
@@ -1,12 +1,26 @@
 import React, { useState } from 'react';
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
 
 function PostDetailPage() {
     const user = useSelector(store => store.user);
 
     return (
         <div>
-            <h2>Post Detail Page goes here</h2>
+            <div>
+                <h2>Post title goes here</h2>
+                <p> Post body goes here</p>
+
+                {/* {user.id === post.user_id && <button>Edit</button>}
+                {user.id === post.user_id && <button>Delete</button>} */}
+            </div>
+
+            <div>
+                <p>COMMENT FORM WILL GO HERE</p>
+            </div>
+
+            <div>
+                <p>COMMENTS WILL GO HERE</p>
+            </div>
         </div>
     );
 }
