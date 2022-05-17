66987024cdbaa6f0289f9aad1bfe004c49982bc0
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-05-17 11:59:04 -0500

    Create roster.router

---
+++ server/routes/roster.router.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+const {
+    rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
+
+router.get('/', rejectUnauthenticated, (req, res) => {
+
+    const query = `SELECT * FROM "user"
+    ORDER BY "access_level" DESC, "username";`;
+    pool.query(query)
+        .then(result => {
+            res.send(result.rows);
+        })
+        .catch(err => {
+            console.log('ERROR GETTING ROSTER IN ROUTER', err);
+            res.sendStatus(500)
+        })
+
+});
+
+module.exports = router;
