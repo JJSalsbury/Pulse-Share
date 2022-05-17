20e733ef7bf5c8ed7f7e76ff1b34017085e819a1
Freddy Reza <rezafreddy@gmail.com>
2022-05-17 11:31:52 -0500

    add inputs to add post page

--- src/components/_AddPostPage/AddPostPage.jsx
+++ src/components/_AddPostPage/AddPostPage.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector, useDispatch} from 'react-redux';
 import { useHistory } from 'react-router-dom';
+
+import './AddPostPage.css'
 
 // imports for file upload
 import 'react-dropzone-uploader/dist/styles.css'
@@ -18,7 +20,10 @@ function AddPostPage() {
     const user = useSelector((store) => store.user);
     const image = useSelector(store => store.image);
 
-    let imageUrl = '';
+    const [postTitle, setPostTitle] = useState('');
+    const [postBody, setPostBody] = useState('');
+
+    // let imageUrl = '';
     
     // specify upload params and url for your files
     const getUploadParams = ({ meta }) => { return { url: 'https://httpbin.org/post' } }
@@ -28,9 +33,8 @@ function AddPostPage() {
     
     // receives array of files that are done uploading when submit button is clicked
     const handleSubmit = async (files, allFiles) => {
-        for(let i = 0; i < files.length; i++) {
-        console.log(files[i])
-        const fileToUpload = files[i];
+        console.log(files[0])
+        const fileToUpload = files[0];
         console.log(fileToUpload['file']);
 
         // get secure url from our server
@@ -46,14 +50,14 @@ function AddPostPage() {
             body: fileToUpload['file']
         })
 
-        imageUrl = url.split('?')[0]
+        const imageUrl = url.split('?')[0]
         console.log(imageUrl)
 
         // dispatch({
         //   type: 'SET_IMAGE',
         //   payload: imageUrl
         // })
-        }
+        
 
         // Empties Dropzone 
         console.log(files.map(f => f.meta))
@@ -62,23 +66,51 @@ function AddPostPage() {
 
     return (
         <div className="container">
-            <h2>Welcome, {user.username}!</h2>
-            <p>Your ID is: {user.id}</p>
-            <LogOutButton className="btn" />
+            <h2>Add Post Page!</h2>
+            <div className="postContainer">
+                <div>
+                    <label htmlFor="post title">
+                    Title:
+                    <input
+                        type="text"
+                        name="post title"
+                        required
+                        value={postTitle}
+                        onChange={(event) => setPostTitle(event.target.value)}
+                    />
+                    </label>
+                </div>
+                <div>
+                <label htmlFor="post body">
+                    Body:
+                    <input
+                        type="text"
+                        name="post body"
+                        required
+                        value={postBody}
+                        onChange={(event) => setPostBody(event.target.value)}
+                    />
+                    </label>
+                </div>
+            </div>
 
             <Dropzone
                 getUploadParams={getUploadParams}
                 onChangeStatus={handleChangeStatus}
                 onSubmit={handleSubmit}
-                maxFiles={3}
-                inputContent={(files, extra) => (extra.reject ? 'Image, audio and video files only' : 'Click Here or Drag 1 Picture/Video')}
+                maxFiles={1}
+                inputContent={(files, extra) => (extra.reject ? 
+                    'Image and video files only' 
+                    : 
+                    'Click Here or Drag 1 Picture/Video'
+                )}
                 styles={{
                 dropzoneReject: { borderColor: 'red', backgroundColor: '#DAA' },
                 inputLabel: (files, extra) => (extra.reject ? { color: 'red' } : {}),
-                dropzone: { width: 400, minHeight: 300, maxHeight: 350 },
+                dropzone: { width: 250, minHeight: 180, maxHeight: 180 },
                 dropzoneActive: { borderColor: "green" }
                 }}
-                accept="image/*,audio/*,video/*"
+                accept="image/*,video/*"
             />
             <img src={image}/>
             <ReactPlayer 
