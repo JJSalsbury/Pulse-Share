e0391854c1626d830301ceb605a819b7b1120f56
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-05-17 16:07:43 -0500

    Import MUI, create TableContainer

--- src/components/_AdminPage/AdminPage.jsx
+++ src/components/_AdminPage/AdminPage.jsx
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import RosterMember from '../_RosterMember/RosterMember';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 function AdminPage() {
     const user = useSelector(store => store.user);
@@ -16,6 +23,7 @@ function AdminPage() {
     return (
         <div>
             <h2>Admin Page goes here</h2>
+            <TableContainer component={Paper}>
             <table>
                 <th>Username</th>
                 <th>Promote/Demote</th>
@@ -29,6 +37,7 @@ function AdminPage() {
                 )
             })}
             </table>
+            </TableContainer>
         </div>
     );
 }
