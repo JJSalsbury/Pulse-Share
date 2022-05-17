01a7eca3e060ec04d9e1183929cabeb4314b23f3
Josh Salsbury <joshuajsalsbury@gmail.com>
2022-05-17 11:36:24 -0500

    Created: comment.router.js/CommentForm.jsx, Server.js; add: comment route requirement, app.use routes

--- server/server.js
+++ server/server.js
@@ -12,7 +12,8 @@ const s3 = require('./s3');
 
 // Route includes
 const userRouter = require('./routes/user.router');
-const profileRouter = require('./routes/profile.router')
+const profileRouter = require('./routes/profile.router');
+const commentRouter = require('./routes/comment.router')
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -28,6 +29,7 @@ app.use(passport.session());
 /* Routes */
 app.use('/api/user', userRouter);
 app.use('/profile', profileRouter);
+app.use('/comment', commentRouter);
 
 // GET route for aws bucket url
 app.get('/s3Url', async (req, res) => {
