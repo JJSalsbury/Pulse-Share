48659537c988a3df08afbcb12e4b19fe228a3667
Brandon McCarty <brandon.m.mccarty12@gmail.com>
2022-05-17 15:26:08 -0500

    add basic styling for post detail page

--- src/components/_PostDetailPage/PostDetailPage.jsx
+++ src/components/_PostDetailPage/PostDetailPage.jsx
@@ -9,7 +9,7 @@ import styling from './PostDetailPage.css'
 import ReactPlayer from 'react-player'
 
 // Material UI
-import {Box, Button} from '@mui/material'
+import { Box, Button, Container, Paper } from '@mui/material'
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 
@@ -17,45 +17,82 @@ function PostDetailPage() {
 
     const dispatch = useDispatch();
     const history = useHistory();
-    const {id} = useParams();
+    const { id } = useParams();
 
     const user = useSelector(store => store.user);
     const post = useSelector(store => store.post);
-    
+
 
     useEffect(() => {
         dispatch({ type: 'GET_POST', payload: id });
-      }, [id]);
+    }, [id]);
 
-      const sendToProfile = () => {
+    const sendToProfile = () => {
         history.push(`/profile/${post.user_id}`)
-      }
+    }
 
-      console.log('POST IS', post);
+    console.log('POST IS', post);
     return (
-        <Box>
-            <Box>
-                <h2>{post.title}</h2>
-                <p>{post.date} {post.time}</p>
+        <Container>
+
+            <Box
+                component={Paper}
+                sx={{
+                    border: '1px solid black',
+                    borderRadius: '7px',
+                    padding: '15px',
+                }}
+            >
+
+                <Box
+                
+                >
+                    <a onClick={sendToProfile}>
+                        <img className='profile-pic' src={post.profile_picture} alt="profile picture" />
+                        <br />
+                        {post.username}</a>
+                </Box>
 
                 <Box>
-                    <img className='profile-pic' src={post.profile_picture} alt="profile picture"/>
-                    <br />
-                    <a onClick={sendToProfile}>{post.username}</a>
+                    <h2>{post.title}</h2>
+                    <p>{post.date} {post.time}</p>
                 </Box>
-                
-                <p>{post.post}</p>
 
-                <img src={post.media} />
-                <ReactPlayer 
-                url={post.media}
-                width='400px'
-                height='600px'
-                controls = {true}/>
 
-                {user.id === post.user_id && <Button variant="contained"><EditIcon /> Edit </Button>}
-                {user.id === post.user_id && <Button variant="contained"><DeleteIcon /> Delete </Button>}
+                <Box>
+                    <p>{post.post}</p>
+                </Box>
+                {/* <img src={post.media} />
+                    <ReactPlayer
+                        url={post.media}
+                        width='400px'
+                        height='600px'
+                        controls={true} /> */}
+
+                <Box className="btn-holder">
+                    {user.id === post.user_id &&
+                        <Button
+                            sx={{
+                                backgroundColor: '#4E9BB9',
+                                margin: '2px'
+                            }}
+                            variant="contained"
+                            className='buttons'
+                        ><EditIcon /> Edit </Button>}
+                    {user.id === post.user_id &&
+                        <Button
+                            variant="contained"
+                            className='buttons'
+                            sx={{
+                                backgroundColor: 'red',
+                                margin: '2px'
+                            }}
+                        ><DeleteIcon /> Delete </Button>}
+                </Box>
+
             </Box>
+
+
 
             <div>
                 <p>COMMENT FORM WILL GO HERE</p>
@@ -64,7 +101,10 @@ function PostDetailPage() {
             <div>
                 <p>COMMENTS WILL GO HERE</p>
             </div>
-        </Box>
+
+
+        </Container>
+
     );
 }
 
