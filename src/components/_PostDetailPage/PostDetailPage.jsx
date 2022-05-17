eb409e296d961787b89fbc3898be2ebab22b5feb
Brandon McCarty <brandon.m.mccarty12@gmail.com>
2022-05-17 14:15:41 -0500

    add material ui

--- src/components/_PostDetailPage/PostDetailPage.jsx
+++ src/components/_PostDetailPage/PostDetailPage.jsx
@@ -1,10 +1,22 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useParams } from 'react-router-dom'
+import { useParams, useHistory } from 'react-router-dom'
+
+// Basic CSS
+import styling from './PostDetailPage.css'
+
+// import for playing videos on dom
+import ReactPlayer from 'react-player'
+
+// Material UI
+import {Box, Button} from '@mui/material'
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function PostDetailPage() {
 
     const dispatch = useDispatch();
+    const history = useHistory();
     const {id} = useParams();
 
     const user = useSelector(store => store.user);
@@ -15,23 +27,35 @@ function PostDetailPage() {
         dispatch({ type: 'GET_POST', payload: id });
       }, [id]);
 
+      const sendToProfile = () => {
+        history.push(`/profile/${post.user_id}`)
+      }
+
       console.log('POST IS', post);
     return (
-        <div>
-            <div>
+        <Box>
+            <Box>
                 <h2>{post.title}</h2>
                 <p>{post.date} {post.time}</p>
 
-                <div>
-                    {post.profile_picture}
-                    {post.username}
-                </div>
+                <Box>
+                    <img className='profile-pic' src={post.profile_picture} alt="profile picture"/>
+                    <br />
+                    <a onClick={sendToProfile}>{post.username}</a>
+                </Box>
                 
                 <p>{post.post}</p>
 
-                {user.id === post.user_id && <button>Edit</button>}
-                {user.id === post.user_id && <button>Delete</button>}
-            </div>
+                <img src={post.media} />
+                <ReactPlayer 
+                url={post.media}
+                width='400px'
+                height='600px'
+                controls = {true}/>
+
+                {user.id === post.user_id && <Button variant="contained"><EditIcon /> Edit </Button>}
+                {user.id === post.user_id && <Button variant="contained"><DeleteIcon /> Delete </Button>}
+            </Box>
 
             <div>
                 <p>COMMENT FORM WILL GO HERE</p>
@@ -40,7 +64,7 @@ function PostDetailPage() {
             <div>
                 <p>COMMENTS WILL GO HERE</p>
             </div>
-        </div>
+        </Box>
     );
 }
 
