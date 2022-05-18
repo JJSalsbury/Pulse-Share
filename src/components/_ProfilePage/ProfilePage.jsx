fc0916ed34cb2b751973c28fc334c466b258d06b
Andrew Lounsbury <lounsburya@gmail.com>
2022-05-18 11:06:33 -0500

    put each section of profile page in box and container

--- src/components/_ProfilePage/ProfilePage.jsx
+++ src/components/_ProfilePage/ProfilePage.jsx
@@ -3,8 +3,10 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
 import { Grid } from '@mui/material';
-import { Radio } from '@mui/material';
-import { Card, CardMedia, CardContent } from '@mui/material';
+import { Container } from '@mui/material';
+import { Box } from '@mui/material';
+
+
 
 
 
@@ -49,39 +51,53 @@ function ProfilePage() {
             <div>
 
                 {/* this goes on the side bar */}
+                <Box sx={{ p: 2, border: '1px solid black'}}>
+                    <Container>
+                        <Box sx={{ p: 2}}>"Profile pic"{profile.profile_picture}</Box>
+                        <Box sx={{ p: 2 }}>{user.username}</Box>
+                        <h2>Contact Info</h2>
+                        <Box sx={{ p: 2}}>{user.email}</Box>
 
-                <p>"profile image place holder p tag"</p>
-                <div>{user.username}</div>
-                <h2>Contact Info</h2>
-                <div>{user.email}</div>
-
-                <div>
-                    <button onClick={handleClick}>Post History</button>
-                    <button onClick={handleEdit}>Edit Profile</button>
-                </div>
+                        <Box sx={{ p: 2 }}>
+                            <button onClick={handleClick}>Post History</button>
+                            <button onClick={handleEdit}>Edit Profile</button>
+                        </Box>
+                    </Container>
+                </Box>
                 {/* this will be a text box center top */}
-                <h2>About Me</h2>
-                <div>{profile.about_me}</div>
+                <Box sx={{ p: 2, border: '1px solid black'}}>
+                <Container>
+                    <h2>About Me</h2>
+                    <box>{profile.about_me}</box>
+                </Container>
+                </Box>
 
                 {/* device and Biometrics placement left of Device settings*/}
-                <h2>My Device</h2>
-                <div>{profile.device}</div>
-
-                <h2>Biometrics</h2>
-                <div>Age: {profile.age}</div>
-                <div>Height: {profile.height}</div>
-                <div>Weight: {profile.weight}</div>
-                <div>Biological Gender: {profile.biological_gender}</div>
-                <div>Injury Level: {profile.injury_level}</div>
-                <div>Aisa Level: {profile.aisa_level}</div>
-                <div>Time Since Injury: {profile.time_since_injury}</div>
-
-
+                <Box sx={{ p: 2, border: '1px solid black'}}>
+                <Container>
+                    <h2>My Device</h2>
+                    <div>{profile.device}</div>
+                </Container>
+                </Box>
+                <Box sx={{ p: 2, border: '1px solid black'}}>
+                <Container>
+                    <h2>Biometrics</h2>
+                    <div>Age: {profile.age}</div>
+                    <div>Height: {profile.height}</div>
+                    <div>Weight: {profile.weight}</div>
+                    <div>Biological Gender: {profile.biological_gender}</div>
+                    <div>Injury Level: {profile.injury_level}</div>
+                    <div>Aisa Level: {profile.aisa_level}</div>
+                    <div>Time Since Injury: {profile.time_since_injury}</div>
+                </Container>
+                </Box>
+                <Box sx={{ p: 2, border: '1px solid black'}}>
                 {/* Device settings is a lager text box placement to the right of device and biometrics */}
-                <h2>Device Settings</h2>
-                <div>{profile.device_settings}</div>
-
-
+                <Container>
+                    <h2>Device Settings</h2>
+                    <div>{profile.device_settings}</div>
+                </Container>
+                </Box>
 
 
 
