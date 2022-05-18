7bf603c5c4c76f0440ba7960fb2bed9f162d8368
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-05-18 14:25:57 -0500

    Add SweetAlerts to Promote and Demote functions

--- src/components/_RosterMember/RosterMember.jsx
+++ src/components/_RosterMember/RosterMember.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import {useHistory} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -9,6 +9,7 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
+import Swal from 'sweetalert2';
 import './RosterMember.css'
 
 
@@ -23,9 +24,9 @@ function RosterMember({ member }) {
         if (member.access_level === 2) {
             return <p></p>;
         } else if (member.access_level === 1) {
-            return <Button sx={{ backgroundColor: 'red'}} variant={'contained'} onClick={demoteUser}>DEMOTE</Button>;
+            return <Button sx={{ backgroundColor: 'red' }} variant={'contained'} onClick={demoteUser}>DEMOTE</Button>;
         } else if (member.access_level === 0) {
-            return <Button sx={{ backgroundColor: 'green'}} variant={'contained'} onClick={promoteUser}>PROMOTE</Button>;
+            return <Button sx={{ backgroundColor: 'green' }} variant={'contained'} onClick={promoteUser}>PROMOTE</Button>;
         }
 
     }
@@ -34,11 +35,38 @@ function RosterMember({ member }) {
         console.log('clicked demote');
         const id = member.id;
         const user = {
-            id: id 
+            id: id
         }
-        dispatch({
-            type: 'DEMOTE_USER',
-            payload: user
+        Swal.fire({
+            title: `Are you sure you want to demote ${member.username} to a normal user?`,
+            text: "Click OK to Demote",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Yes, Demote!',
+            cancelButtonText: 'No, Cancel!',
+            reverseButtons: true
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                dispatch({
+                    type: 'DEMOTE_USER',
+                    payload: user
+                })
+                Swal.fire(
+                    'Demoted!',
+                    `You revoked ${member.username}'s Moderator privileges.`,
+                    'success'
+                )
+            } else if (
+                /* Read more about handling dismissals below */
+                result.dismiss === Swal.DismissReason.cancel
+            ) {
+                Swal.fire(
+                    'Cancelled',
+                    `${member.username} will remain a Moderator.`,
+                    'error'
+                )
+            }
         })
     }
 
@@ -46,19 +74,49 @@ function RosterMember({ member }) {
         console.log('clicked promote');
         const id = member.id;
         const user = {
-            id: id 
+            id: id
         }
-        dispatch({
-            type: 'PROMOTE_USER',
-            payload: user
+
+        Swal.fire({
+            title: `Are you sure you want to promote ${member.username} to a Moderator?`,
+            text: "Click OK to Promote",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Yes, Promote!',
+            cancelButtonText: 'No, Cancel!',
+            reverseButtons: true
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                dispatch({
+                    type: 'PROMOTE_USER',
+                    payload: user
+                })
+                Swal.fire(
+                    'Promoted!',
+                    `You have promoted ${member.username} to a Moderator.`,
+                    'success'
+                )
+            } else if (
+                /* Read more about handling dismissals below */
+                result.dismiss === Swal.DismissReason.cancel
+            ) {
+                Swal.fire(
+                    'Cancelled',
+                    `${member.username} will remain a normal user.`,
+                    'error'
+                )
+            }
         })
+
+
     }
 
     const deleteUser = () => {
         console.log('clicked delete');
         const id = member.id;
         const user = {
-            id: id 
+            id: id
         }
         dispatch({
             type: 'DELETE_USER',
@@ -69,9 +127,9 @@ function RosterMember({ member }) {
     return (
         <>
             <TableRow>
-                <TableCell align={'center'}><a onClick={() => {history.push(`/profile/${member.id}`)}}>{member.username}</a></TableCell>
+                <TableCell align={'center'}><a onClick={() => { history.push(`/profile/${member.id}`) }}>{member.username}</a></TableCell>
                 <TableCell align={'center'}>{buttonRender()}</TableCell>
-                <TableCell align={'center'}>{member.access_level < 2 ? <Button sx={{ backgroundColor: 'black'}} variant={'contained'} onClick={deleteUser}>DELETE</Button> : <p></p>}</TableCell>
+                <TableCell align={'center'}>{member.access_level < 2 ? <Button sx={{ backgroundColor: 'black' }} variant={'contained'} onClick={deleteUser}>DELETE</Button> : <p></p>}</TableCell>
             </TableRow>
         </>
     )
