9332bdc1845729b3d29bb3742f764bfe72de98f6
Josh Salsbury <joshuajsalsbury@gmail.com>
2022-05-19 17:41:45 -0500

    created delete route, created deleteComment function to trigger on click. Revised payload in comment saga getComments and deleteComments generator functions

--- src/components/_CommentItem/CommentItem.jsx
+++ src/components/_CommentItem/CommentItem.jsx
@@ -1,40 +1,24 @@
+//Imports
 import React from 'react';
-// import * as React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-// import Card from '@material-ui/core/Card';
-// import CardContent from '@material-ui/core/CardContent';
-// import CardMedia from '@material-ui/core/CardMedia';
-// import Typography from '@material-ui/core/Typography';
-// import { Button, CardActionArea, CardActions } from '@material-ui/core';
-// import { makeStyles } from '@material-ui/core/styles';
-
-import { Box, Button, Container, ListItemAvatar, Avatar, Typography, Divider, Paper } from '@mui/material';
+import { useHistory, useParams } from 'react-router-dom';
+//Styling Imports 
+import { Box, Button, Container, ListItemAvatar, Avatar, Typography, Divider, ListItem } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
-import ListItem from '@mui/material/ListItem';
+import Swal from 'sweetalert2';
 
 
 
-// const useStyles = makeStyles({
-//     root: {
-//         maxWidth: 300,
-//         margin: "3rem",
-//     },
-// });
-
-
-function CommentItem({ comment }) {
+function CommentItem({ comment, postId }) {
     const user = useSelector(store => store.user);
     const dispatch = useDispatch();
+    const history = useHistory();
+    const { id } = useParams();
+    console.log('user:', user, 'id:', id);
 
-    const history = useHistory()
 
-    // const classes = useStyles();
 
-    // useEffect(() => {
-    //     dispatch({ type: 'GET_COMMENTS', payload: comment.post_id});
-    // }, [id]);
 
     // const handleDetails = () => {
     //     // console.log('clicked for comment details (description)');
@@ -42,41 +26,42 @@ function CommentItem({ comment }) {
     //     history.push('/details');
     // }
 
-    // style={{width: 175, height: 375}}
-    // style={{width: 175, height: 100}}  
+    // Delete the post
+    const deleteComment = () => {
 
-    //Render return all comments in DB
+        Swal.fire({
+            title: `Are you sure you want to delete this post?`,
+            text: `This action cannot be undone.`,
+            icon: 'warning',
+            background: 'white',
+            color: 'black',
+            showCancelButton: true,
+            confirmButtonColor: '#4E9BB9',
+            cancelButtonColor: 'red',
+            confirmButtonText: 'Delete'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                dispatch({type: 'DELETE_COMMENT', payload: {id:comment.id, post_id: postId}})
+                history.push(`/postDetail/${id}`) 
+                Swal.fire({
+                    background: 'white',
+                    color: 'black',
+                    confirmButtonColor: '#4E9BB9',
+                    title: 'Deleted!',
+                    text: `Post has been deleted.`,
+                    icon: 'success'
+                })
+            }
+        })
+    }
+
+    console.log('COMMENT:', comment.comment);
+
     return (
         <ListItem alignItems="flex-start">
             <Container>
-                {/* <Box
-                component={Paper}
-                sx={{
-                    border: '1px solid black',
-                    borderRadius: '7px',
-                    padding: '15px',
-                }}
-            > */}
-                {/* <ListItemAvatar >
-                <Avatar alt=“Remy Sharp” src=“/static/images/avatar/1.jpg” />
-                <Typography
-                        component=“span”
-                        variant=“body2”
-                        color=“text.primary”
-                    >
-                        {post.username}
-                    </Typography>
-                </ListItemAvatar> */}
-                                    {/* <Box
-                        sx={{
-                            margin: '15px',
-                            marginTop: '26px',
-                            textAlign: 'center'
-                        }}
-                    > */}
-
                 <ListItemAvatar> {comment.profile_picture == '' ?
-                    <Avatar alt="profile picture" src="https://st3.depositphotos.com/6672868/13701/v/600/depositphotos_137014128-stock-illustration-user-profile-icon.jpg" alt="profile picture" /> :
+                    <Avatar alt="profile picture" src="https://st3.depositphotos.com/6672868/13701/v/600/depositphotos_137014128-stock-illustration-user-profile-icon.jpg" /> :
                     <Avatar alt="profile picture" src={comment.profile_picture} />}
                 </ListItemAvatar>
                 {/* </Box> */}
@@ -84,7 +69,7 @@ function CommentItem({ comment }) {
                     <Typography
                         sx={{ display: 'inline' }}
                         component="span"
-                        variant="body2"
+                        variant="body1"
                         color="text.primary"
                     >
                         {comment.username}
@@ -116,6 +101,7 @@ function CommentItem({ comment }) {
                     <Button
                         variant="contained"
                         className='buttons'
+                        onClick={deleteComment}
                         sx={{
                             backgroundColor: 'red',
                             margin: '2px',
--- src/redux/sagas/comment.saga.js
+++ src/redux/sagas/comment.saga.js
@@ -5,7 +5,8 @@ function* addComment(action) {
     //add comment to DB
     try{
         console.log('Payload in addComment:', action.payload);
-        const comment = yield axios.post('/comment', action.payload)
+        const comment = yield axios.post('/comment', action.payload);
+        yield put({type: 'GET_COMMENTS', payload: action.payload.post_id})
         console.log('added comment:', comment.data)
     } catch (error) {
         console.log('addComment error:', error)
@@ -17,15 +18,27 @@ function* getComments(action) {
     try{
         const comments = yield axios.get(`/comment/${action.payload}`);
         yield put({ type: 'SET_COMMENTS', payload: comments.data });
-        console.log('GET Comments:', comments.data)
+        console.log('GET Comments SAGA:', comments.data)
     } catch (error) {
         console.log('GET Comments error:', error)
+    }
+}
+
+// Delete selected comment
+function* deleteComment(action) {
+    try {
+        yield axios.delete(`/comment/${action.payload.id}`)
+        yield put({type: 'GET_COMMENTS', payload: action.payload.post_id})
+        console.log('DELETE Comment SAGA:', action.payload);
+    } catch (error) {
+        console.log(error);
     }
 }
 
 function* commentSaga() {
     yield takeLatest('CREATE_NEW_COMMENT', addComment);
     yield takeLatest('GET_COMMENTS', getComments);
+    yield takeLatest('DELETE_COMMENT', deleteComment);
 }
 
 export default commentSaga;
