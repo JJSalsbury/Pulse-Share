922a3c672f1d28c7b908b41fd297f9523e90215c
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-05-19 14:00:58 -0500

    Move history.push from entire post to 'View Post' button

--- src/components/_HistoryItem/HistoryItem.jsx
+++ src/components/_HistoryItem/HistoryItem.jsx
@@ -3,18 +3,19 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import Swal from 'sweetalert2';
 import { Box, Button, Container, Paper } from '@mui/material'
-import EditIcon from '@mui/icons-material/Edit';
+import VisibilityIcon from '@mui/icons-material/Visibility';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 
 
 function HistoryItem({ post }) {
 
+    const history = useHistory();
+
     return (
         <>
             <Box
                 component={Paper}
-                onClick={() => {history.push(`/postDetail/${post.id}`)}}
                 sx={{
                     border: '1px solid black',
                     borderRadius: '7px',
@@ -30,14 +31,23 @@ function HistoryItem({ post }) {
                 </Box>
 
                 <Box className="btn-holder">
-                        <Button
-                            variant="contained"
-                            className='buttons'
-                            sx={{
-                                backgroundColor: 'red',
-                                margin: '2px'
-                            }}
-                        ><DeleteIcon /> Delete </Button>
+                    <Button
+                        onClick={() => { history.push(`/postDetail/${post.id}`) }}
+                        sx={{
+                            backgroundColor: '#4E9BB9',
+                            margin: '2px'
+                        }}
+                        variant="contained"
+                        className='buttons'
+                    ><VisibilityIcon /> VIEW POST </Button>
+                    <Button
+                        variant="contained"
+                        className='buttons'
+                        sx={{
+                            backgroundColor: 'red',
+                            margin: '2px'
+                        }}
+                    ><DeleteIcon /> Delete </Button>
                 </Box>
             </Box>
         </>
