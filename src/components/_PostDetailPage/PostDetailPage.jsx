e576ed9ca702dba4c2ab7f6d701a8faadec7edc2
Brandon McCarty <brandon.m.mccarty12@gmail.com>
2022-05-19 14:45:34 -0500

    setup edit post

--- src/components/_PostDetailPage/PostDetailPage.jsx
+++ src/components/_PostDetailPage/PostDetailPage.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams, useHistory } from 'react-router-dom'
+import EditPost from '../_EditPost/EditPost';
 
 // Basic CSS
 import styling from './PostDetailPage.css'
@@ -12,7 +13,7 @@ import Swal from 'sweetalert2';
 import ReactPlayer from 'react-player'
 
 // Material UI
-import { Box, Button, Container, Paper } from '@mui/material'
+import { Box, Button, Container, Paper, Avatar } from '@mui/material'
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 
@@ -25,6 +26,8 @@ function PostDetailPage() {
     const user = useSelector(store => store.user);
     const post = useSelector(store => store.post);
 
+    const [editMode, setEditMode] = useState(false);
+
 
     useEffect(() => {
         dispatch({ type: 'GET_POST', payload: id });
@@ -33,6 +36,12 @@ function PostDetailPage() {
     // Navigate to poster's profile
     const sendToProfile = () => {
         history.push(`/profile/${post.user_id}`)
+    }
+
+    // Edit selected post
+    const editPost = () => {
+        console.log('Editing post', id);
+        setEditMode(true);
     }
 
     // Delete the post
@@ -50,8 +59,8 @@ function PostDetailPage() {
             confirmButtonText: 'Delete'
         }).then((result) => {
             if (result.isConfirmed) {
-                dispatch({type: 'DELETE_POST', payload: id})
-                history.push('/posts') 
+                dispatch({ type: 'DELETE_POST', payload: id })
+                history.push('/posts')
                 Swal.fire({
                     background: 'white',
                     color: 'black',
@@ -64,101 +73,113 @@ function PostDetailPage() {
         })
     }
 
-    console.log('POST IS', post);
+
     return (
+
         <Container>
 
-            <Box
-                component={Paper}
-                sx={{
-                    border: '1px solid black',
-                    borderRadius: '7px',
-                    padding: '15px',
-                }}
-            >
+            <div>
+                {editMode ?
+
+                <EditPost 
+                    setEditMode={setEditMode}
+                    postId={id}
+                />
+
+                :
 
                 <Box
+                    component={Paper}
                     sx={{
-                        justifyContent: 'space-evenly',
-                        display: 'flex'
+                        border: '1px solid black',
+                        borderRadius: '7px',
+                        padding: '15px',
                     }}
                 >
+
                     <Box
                         sx={{
-                            margin: '15px',
-                            marginTop: '26px',
-                            textAlign: 'center'
+                            justifyContent: 'space-evenly',
+                            display: 'flex'
                         }}
                     >
-                        <a onClick={sendToProfile}>
-                            {post.profile_picture == '' ?
-                            <img className='profile-pic' src="https://st3.depositphotos.com/6672868/13701/v/600/depositphotos_137014128-stock-illustration-user-profile-icon.jpg" alt="profile picture" /> :
-                            <img className='profile-pic' src={post.profile_picture} alt="profile picture" />  
-                            }
-                            <br />
-                            <h3>{post.username}</h3></a>
-                    </Box>
-
-                    <Box>
-                        <h2>{post.title}</h2>
-                        <p>{post.date} {post.time}</p>
-
-                        <p>{post.post}</p>
-                    </Box>
-                </Box>
-
-                <Box
-                    sx={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        margin: '15px'
-                    }}
-                >
-                    <Box>
-                        {post.video &&
-                            <ReactPlayer
-                                url={post.video}
-                                width='640px'
-                                height='360px'
-                                controls={true} />}
-                    </Box>
-                    <br />
-                    <Box>
-                        {post.image &&
-                            <img
-                                src={post.image}
-                                maxheight='360px'
-                                maxwidth='640px'
-                            />}
-                    </Box>
-                    
-                </Box>
-
-                <Box className="btn-holder">
-                    {user.id === post.user_id &&
-                        <Button
+                        <Box
                             sx={{
-                                backgroundColor: '#4E9BB9',
-                                margin: '2px'
+                                margin: '15px',
+                                marginTop: '26px',
+                                textAlign: 'center'
                             }}
-                            variant="contained"
-                            className='buttons'
-                        ><EditIcon /> Edit </Button>}
-                    {(user.access_level >= 1 || user.id === post.user_id) &&
-                        <Button
-                            variant="contained"
-                            className='buttons'
-                            onClick={deletePost}
-                            sx={{
-                                backgroundColor: 'red',
-                                margin: '2px'
-                            }}
-                        ><DeleteIcon /> Delete </Button>}
+                        >
+                            <a onClick={sendToProfile}>
+                                {post.profile_picture == '' ?
+                                    <img className='profile-pic' src="https://st3.depositphotos.com/6672868/13701/v/600/depositphotos_137014128-stock-illustration-user-profile-icon.jpg" alt="profile picture" /> :
+                                    <img className='profile-pic' src={post.profile_picture} alt="profile picture" />
+                                }
+                                <br />
+                                <h3>{post.username}</h3></a>
+                        </Box>
+
+                        <Box>
+                            <h2>{post.title}</h2>
+                            <p>{post.date} {post.time}</p>
+
+                            <p>{post.post}</p>
+                        </Box>
+                    </Box>
+
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            margin: '15px'
+                        }}
+                    >
+                        <Box>
+                            {post.video &&
+                                <ReactPlayer
+                                    url={post.video}
+                                    width='640px'
+                                    height='360px'
+                                    controls={true} />}
+                        </Box>
+                        <br />
+                        <Box>
+                            {post.image &&
+                                <img
+                                    src={post.image}
+                                    maxheight='360px'
+                                    maxwidth='640px'
+                                />}
+                        </Box>
+
+                    </Box>
+
+                    <Box className="btn-holder">
+                        {user.id === post.user_id &&
+                            <Button
+                                sx={{
+                                    backgroundColor: '#4E9BB9',
+                                    margin: '2px'
+                                }}
+                                variant="contained"
+                                className='buttons'
+                                onClick={editPost}
+                            ><EditIcon /> Edit </Button>}
+                        {(user.access_level >= 1 || user.id === post.user_id) &&
+                            <Button
+                                variant="contained"
+                                className='buttons'
+                                onClick={deletePost}
+                                sx={{
+                                    backgroundColor: 'red',
+                                    margin: '2px'
+                                }}
+                            ><DeleteIcon /> Delete </Button>}
+                    </Box>
+
                 </Box>
-
-            </Box>
-
-
+            }
+            </div>
 
             <div>
                 <p>COMMENT FORM WILL GO HERE</p>
--- src/redux/reducers/_editProfile.reducer.js
+++ src/redux/reducers/_editProfile.reducer.js
@@ -2,17 +2,12 @@ const editProfileReducer = (state={}, action) => {
     if (action.type === 'SET_PROFILE_T0_EDIT') {
         console.log(action.payload);
         return action.payload;
-    }  else if (action.type === 'EDIT_PRIVACY') {
+    } else if (action.type === 'EDIT_POST_ON_CHANGE') {
         return {
             ...state, 
             [action.payload.property]: action.payload.value
         };
-    } else if (action.type === 'EDIT_ON_CHANGE') {
-        return {
-            ...state, 
-            [action.payload.property]: action.payload.value
-        };
-    } else if (action.type === 'CLEAR_EDIT') {
+    } else if (action.type === 'CLEAR_EDIT_POST') {
         return {};
     }
     return state;
