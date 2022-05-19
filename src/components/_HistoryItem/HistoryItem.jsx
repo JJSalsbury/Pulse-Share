422736eacf795bd5317ed479275f2e8120d61ae6
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-05-19 11:26:51 -0500

    Create HistoryItem component

---
+++ src/components/_HistoryItem/HistoryItem.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+import Swal from 'sweetalert2';
+
+
+function HistoryItem({ post }) {
+
+    return (
+        <>
+        </>
+    )
+}
+
+
+export default HistoryItem;
