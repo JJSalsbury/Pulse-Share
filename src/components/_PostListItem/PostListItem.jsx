f2548d2a12acc8b6e7faae3faeed4c249c647d2d
Brandon McCarty <brandon.m.mccarty12@gmail.com>
2022-05-20 15:08:51 -0500

    change chip style

--- src/components/_PostListItem/PostListItem.jsx
+++ src/components/_PostListItem/PostListItem.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import {useSelector, useDispatch} from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
 import './PostListItem.css'
@@ -26,13 +26,13 @@ import {
     Chip
 } from '@mui/material';
 
-function PostListItem({post}) {
+function PostListItem({ post }) {
 
     const history = useHistory();
     const dispatch = useDispatch();
 
     const user = useSelector(store => store.user);
-    const outcomesList = useSelector( store => store.outcomesListReducer);
+    const outcomesList = useSelector(store => store.outcomesListReducer);
 
     const handleClick = () => {
         history.push('')
@@ -40,7 +40,7 @@ function PostListItem({post}) {
 
 
     return (
-        <Box 
+        <Box
             component={Paper}
             sx={{
                 padding: '15px',
@@ -50,78 +50,79 @@ function PostListItem({post}) {
                 marginBottom: '15px',
                 minWidth: '60vw',
                 maxWidth: '60vw',
-        }}>
-            <ListItem alignItems="flex-start">
-            <ListItemAvatar 
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                paddingRight: 3
             }}>
-                <Avatar className="hoverForPointer" alt="Profile Picture" src={post.profile_picture} onClick={() => { history.push(`/profile/${post.user_id}`) }}/>
-                <Typography
+            <ListItem alignItems="flex-start">
+                <ListItemAvatar
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        paddingRight: 3
+                    }}>
+                    <Avatar className="hoverForPointer" alt="Profile Picture" src={post.profile_picture} onClick={() => { history.push(`/profile/${post.user_id}`) }} />
+                    <Typography
                         component="span"
                         variant="body2"
                         color="text.primary"
+                    >
+                        <a onClick={() => { history.push(`/profile/${post.user_id}`) }}>{post.username}</a>
+                    </Typography>
+                </ListItemAvatar>
+                <Box
+                    className="hoverForPointer"
+                    onClick={() => { history.push(`/postDetail/${post.id}`) }}
+                    sx={{
+                        display: 'flex'
+                    }}
                 >
-                    <a onClick={() => { history.push(`/profile/${post.user_id}`) }}>{post.username}</a>
-                </Typography>
-            </ListItemAvatar>
-            <Box 
-                className="hoverForPointer"
-                onClick={() => { history.push(`/postDetail/${post.id}`) }}
-                sx={{
-                    display: 'flex'
-                }}
-            >
-            <ListItemText
-                sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'space-between'
-                }}
-                primary={
-                    <Box>
-                        <Typography
-                            component="span"
-                            variant="h4"
-                            color="text.primary"
-                            
-                        >
-                            {post.title}
-                        </Typography>
-                        <br/>
-                        <Typography
-                            component="span"
-                            variant="body1"
-                            color="text.primary"
-                        >
-                            {post.post}
-                        </Typography>
-                    </Box>
-                }
-                secondary={
-                    <React.Fragment>
-                    <Box sx={{
-                        marginTop: '10px'
-                    }}>
-                    {post.date} {post.time}
-                    </Box>
-                    
-                    <br/>
-                    <Chip 
-                        label={outcomesList[post.outcome_id - 1]?.outcome} 
-                        variant="outlined" 
+                    <ListItemText
                         sx={{
-                            color: '#4E9BB9',
-                            border: '1px solid #4E9BB9'
-                        }} 
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'space-between'
+                        }}
+                        primary={
+                            <Box>
+                                <Typography
+                                    component="span"
+                                    variant="h4"
+                                    color="text.primary"
+
+                                >
+                                    {post.title}
+                                </Typography>
+                                <br />
+                                <Typography
+                                    component="span"
+                                    variant="body1"
+                                    color="text.primary"
+                                >
+                                    {post.post}
+                                </Typography>
+                            </Box>
+                        }
+                        secondary={
+                            <React.Fragment>
+                                <Box sx={{
+                                    marginTop: '10px'
+                                }}>
+                                    {post.date} {post.time}
+                                </Box>
+
+                                <br />
+                                <Chip
+                                    label={outcomesList[post.outcome_id - 1]?.outcome}
+                                    variant="outlined"
+                                    sx={{
+                                        bgcolor: '#4E9BB9',
+                                        color: 'white',
+                                        border: '1px solid #4E9BB9'
+                                    }}
+                                />
+                            </React.Fragment>
+                        }
                     />
-                    </React.Fragment>
-                }
-                />
-            </Box>
+                </Box>
             </ListItem>
         </Box>
     );
