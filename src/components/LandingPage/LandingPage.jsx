6fc13e8cf61d94df68095481cb8a21b81ddec5a3
Freddy Reza <rezafreddy@gmail.com>
2022-05-26 11:15:43 -0500

    rearrange landing page and add some websites

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -2,11 +2,32 @@ import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import './LandingPage.css';
 
+// imports for MUI v5
+import {
+  Box,
+  Container,
+  Paper,
+  Select,
+  MenuItem,
+  InputLabel,
+  Button,
+  Modal,
+  Typography,
+  TextField,
+  FormControl,
+  Avatar,
+  ListItem,
+  List,
+  Divider,
+  ListItemAvatar,
+  ListItemText
+} from '@mui/material';
+
 // CUSTOM COMPONENTS
 import RegisterForm from '../RegisterForm/RegisterForm';
 
 function LandingPage() {
-  const [heading, setHeading] = useState('Welcome');
+  const [heading, setHeading] = useState('Welcome To Pulse Share');
   const history = useHistory();
 
   const onLogin = (event) => {
@@ -14,12 +35,134 @@ function LandingPage() {
   };
 
   return (
-    <div className="container">
-      <h2>{heading}</h2>
+    <Container 
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <h1>{heading}</h1>
 
-      <div className="grid">
-        <div className="grid-col grid-col_8">
-          <p>
+      <Box 
+        component={Paper}
+          sx={{
+              padding: '15px',
+              borderRadius: '7px',
+              border: '1px solid black',
+              boxShadow: 10,
+              marginTop: '15px',
+              textAlign: 'center'
+              // minWidth: '60vw',
+              // maxWidth: '60vw',
+          }}
+      >
+        <h3>Mission</h3>
+        <Typography>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+          id felis metus. Vestibulum et pulvinar tortor. Morbi pharetra lacus
+          ut ex molestie blandit. Etiam et turpis sit amet risus mollis
+          interdum. Suspendisse et justo vitae metus bibendum fringilla sed
+          sed justo. Aliquam sollicitudin dapibus lectus, vitae consequat odio
+          elementum eget. Praesent efficitur eros vitae nunc interdum, eu
+          interdum justo facilisis. Sed pulvinar nulla ac dignissim efficitur.
+          Quisque eget eros metus. Vestibulum bibendum fringilla nibh a
+          luctus. Duis a sapien metus.
+        </Typography>
+      </Box>
+      <Box 
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          // alignItems: 'center',
+        }}
+      >
+        <Box
+          component={Paper}
+          sx={{
+            padding: '15px',
+            borderRadius: '7px',
+            border: '1px solid black',
+            boxShadow: 10,
+            marginTop: '15px',
+            textAlign: 'center',
+            mr: '5px',
+            minWidth: '50%',
+            maxWidth: '50%',
+          }}
+        >
+          <h3>Learn More</h3>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-around'
+            }}
+          >
+            <Box>
+              <img height='100px' src="https://spinal-stim-forum-test-bucket.s3.us-east-2.amazonaws.com/9a5116b1d45c0833.jpeg" alt="Medtronic Webpage" />
+            </Box>
+            <Box
+              sx={{
+                maxWidth: '400px'
+              }}
+            >
+              <a
+                target="_blank"
+                href='https://www.medtronic.com/us-en/patients/treatments-therapies/spinal-cord-stimulation-chronic-pain/therapy-overview.html?cmpid=PPC_Goog_Q3_adgrp_Gen_H1_Spin_Cord_H2_Watc_the%20_H3_Expl_Trea_SCS_Patient_Pan_Pain_Patient_FY22'
+                sx={{
+                  overflow: 'auto',
+                }}
+              >
+                Medtronic Webpage
+              </a>
+            </Box>
+          </Box>
+          <br/>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-around'
+            }}
+          >
+            <Box>
+              <img height='100px' src="https://spinal-stim-forum-test-bucket.s3.us-east-2.amazonaws.com/fc512b7a98a28f16.jpeg" alt="Medtronic Webpage" />
+            </Box>
+            <Box
+              sx={{
+                maxWidth: '400px'
+              }}
+            >
+              <a
+                target="_blank"
+                href='https://www.bostonscientific.com/en-US/medical-specialties/pain-management/wavewriter-alpha-scs.html?gclsrc=aw.ds&gclid=CjwKCAjwyryUBhBSEiwAGN5OCLjnQ-8BR6_JER4YFdUYuf1682JbsaknXv2rlwYGQffM-_1KwFJvDhoCMi0QAvD_BwE'
+                sx={{
+                  overflow: 'auto',
+                }}
+              >
+                Boston Scientific Webpage
+              </a>
+            </Box>
+          </Box>
+        </Box>
+        <Box
+          component={Paper}
+          sx={{
+            padding: '15px',
+            borderRadius: '7px',
+            border: '1px solid black',
+            boxShadow: 10,
+            marginTop: '15px',
+            textAlign: 'center',
+            ml: '5px',
+            // maxWidth: '60vw',
+          }}
+        >
+          <h3>What is SCS?</h3>
+          <Typography>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
             id felis metus. Vestibulum et pulvinar tortor. Morbi pharetra lacus
             ut ex molestie blandit. Etiam et turpis sit amet risus mollis
@@ -29,40 +172,10 @@ function LandingPage() {
             interdum justo facilisis. Sed pulvinar nulla ac dignissim efficitur.
             Quisque eget eros metus. Vestibulum bibendum fringilla nibh a
             luctus. Duis a sapien metus.
-          </p>
-
-          <p>
-            Praesent consectetur orci dui, id elementum eros facilisis id. Sed
-            id dolor in augue porttitor faucibus eget sit amet ante. Nunc
-            consectetur placerat pharetra. Aenean gravida ex ut erat commodo, ut
-            finibus metus facilisis. Nullam eget lectus non urna rhoncus
-            accumsan quis id massa. Curabitur sit amet dolor nisl. Proin
-            euismod, augue at condimentum rhoncus, massa lorem semper lacus, sed
-            lobortis augue mi vel felis. Duis ultrices sapien at est convallis
-            congue.
-          </p>
-
-          <p>
-            Fusce porta diam ac tortor elementum, ut imperdiet metus volutpat.
-            Suspendisse posuere dapibus maximus. Aliquam vitae felis libero. In
-            vehicula sapien at semper ultrices. Vivamus sed feugiat libero. Sed
-            sagittis neque id diam euismod, ut egestas felis ultricies. Nullam
-            non fermentum mauris. Sed in enim ac turpis faucibus pretium in sit
-            amet nisi.
-          </p>
-        </div>
-        <div className="grid-col grid-col_4">
-          <RegisterForm />
-
-          <center>
-            <h4>Already a Member?</h4>
-            <button className="btn btn_sizeSm" onClick={onLogin}>
-              Login
-            </button>
-          </center>
-        </div>
-      </div>
-    </div>
+          </Typography>
+        </Box>
+      </Box>
+    </Container>
   );
 }
 
